a4c0454765af8506d0aa98cc5b8765e893d372e5
Yarizzer@gmail.com <Yarizzer@gmail.com>
2023-01-19 12:33:24 +0300

    AppCore born

---
+++ Styler/Application/AppCore/AppCore.swift
@@ -0,0 +1,18 @@
+//
+//  AppCore.swift
+//  Styler
+//
+//  Created by Yaroslav Abaturov on 19.01.2023.
+//
+
+import Foundation
+
+class AppCore {
+    static let shared = AppCore()
+    
+    private init() {}
+    
+    func prepareSession() {}
+    func runSession() {}
+    func closeSession() {}
+}
